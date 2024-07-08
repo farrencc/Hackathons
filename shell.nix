4014af7fa01759f202bca780961f5df0c6889a3f
farrencc <farrencc@tcd.ie>
2024-07-08 17:26:03 +0100

    Corrected typo in name and adding in shell.nix to use python etc.

---
+++ shell.nix
@@ -0,0 +1,24 @@
+with import <nixpkgs> {};
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    xorg.libX11
+    libGL
+    gcc
+    cmake
+    gfortran
+    gsl
+    doxygen
+  ];
+
+  packages = [
+  (pkgs.python3.withPackages(python-pkgs: [
+    python-pkgs.numpy
+    python-pkgs.matplotlib
+    python-pkgs.scipy
+    python-pkgs.pandas
+    python-pkgs.geopandas
+  ]))
+  ];
+
+}
